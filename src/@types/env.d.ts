59b0594d52d334268770abdf729412b08925640a
Flávio Arêas <vflavio9@gmail.com>
2022-10-06 14:57:41 -0300

    feat: fetch token with expo auth

---
+++ src/@types/env.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-native-dotenv' {
+  export const GCP_CLIENT_ID: string;
+  export const GCP_REDIRECT_URI: string;
+}
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,46 @@
+import * as AuthSession from 'expo-auth-session';
+
+import { GCP_CLIENT_ID, GCP_REDIRECT_URI } from 'react-native-dotenv';
+
 import { Button } from '../Button';
 import { User } from '../User';
 
 import { Container } from './styles';
 
+type AuthResponse = {
+  params: {
+    access_token: string;
+  };
+  type: string;
+};
+
 export function Form() {
+  async function handleGoogleSignIn() {
+    try {
+      const clientId = GCP_CLIENT_ID;
+      const redirectUri = GCP_REDIRECT_URI;
+      const responseType = 'token';
+      const scope = encodeURI('profile email');
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+
+      const { params, type } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthResponse;
+
+      console.log({ params, type });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <Container>
-      <Button icon="google" title="Entrar com Google" />
+      <Button
+        icon="google"
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+      />
 
       {/* <User /> */}
     </Container>
